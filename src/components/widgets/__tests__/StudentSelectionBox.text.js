63b43d6d67c3c132cb97b8f1ce891ae00776a031
oleg.spakov <oleg.spakov@uta.fi>
2017-08-04 13:04:06 +0300

    jest tests initilized

---
+++ src/components/widgets/__tests__/StudentSelectionBox.text.js
@@ -0,0 +1,27 @@
+import Vue from 'vue';
+import jsdom from 'jsdom';
+import StudentSelectionBox from 'widgets/StudentSelectBox.vue';
+
+const renderer = require('vue-server-renderer').createRenderer();
+
+describe('Test suite for StudentSelectionBox', () => {
+
+  it('Dummy testing...', () => {
+
+    const ClonedComponent = Vue.extend( StudentSelectionBox );
+    const NewComponent = new ClonedComponent({
+      data() {
+        return {
+        };
+      },
+    }).$mount();
+
+    renderer.renderToString( NewComponent, (err, str) => {
+      const dom = new jsdom.JSDOM( str );
+      const selectButton = dom.window.document.querySelector( '.level-left button' );
+      expect( selectButton.textContent ).toContain( 'Select' );
+    });
+
+  });
+
+});
