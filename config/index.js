ee001aecfcdb0cfce52dd2d0456ceb58639c7780
oleg.spakov <oleg.spakov@uta.fi>
2017-08-09 10:54:14 +0300

    production build fixed

--- config/index.js
+++ config/index.js
@@ -7,7 +7,10 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: (() => {
+        const re = /^\"(.*)\"$/g;
+        return require('./prod.env').URL_PATH.replace( re, '$1' );
+    })(),
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
