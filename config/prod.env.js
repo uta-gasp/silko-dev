8d2fd398090e8b765cec5155ea8a8b270cde42cf
oleg.spakov <oleg.spakov@uta.fi>
2018-10-11 15:17:42 +0300

    update

--- config/prod.env.js
+++
@@ -1,9 +0,0 @@
-const isPublishedDev = process.argv[2] === 'dev';
-const url = isPublishedDev ? '"/silko-dev"' : '"/silko"';
-console.log( `Setting up for ${url}, database is ${isPublishedDev ? 'DEV' : 'PROD'}` );
-
-module.exports = {
-  NODE_ENV: '"production"',
-  URL_PATH: url,
-  IS_DEV_DB: isPublishedDev,
-}
--- src/config/db.development.js
+++
@@ -1,8 +0,0 @@
-export const config = {
-  apiKey: 'AIzaSyBeKdNAX1_JHeGnPouQTYH0d3APAYzRZVY',
-  authDomain: 'eye-school-test.firebaseapp.com',
-  databaseURL: 'https://eye-school-test.firebaseio.com',
-  projectId: 'eye-school-test',
-  storageBucket: 'eye-school-test.appspot.com',
-  messagingSenderId: '1082968904935',
-};
